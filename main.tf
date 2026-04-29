f9367b650f85b901427edd742718c0bcba42d166
Sahithi0602 <85388736+Sahithi0602@users.noreply.github.com>
2026-04-29 23:32:18 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "one" {
   ami                    = "ami-098e39bafa7e7303d"
   instance_type          = "c7i-flex.large"
   key_name               = "project-key"
+  subnet_id              = "subnet-07292b9771570a92e"  
   vpc_security_group_ids = ["sg-04dfda1b938b72f0e"]
   tags = {
     Name = var.instance_names[count.index]
@@ -15,4 +16,7 @@ resource "aws_instance" "one" {
 
 variable "instance_names" {
   default = ["ansible", "dev", "test", "Monitoring server"]
+}
+resource "aws_s3_bucket" "demo" {
+  bucket = "sahithi-monolithic-bucket-0622"
 }
